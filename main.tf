7e1fdddec6f4db84473e1dd9c66a8968245ee09d
pmalloja <mallojalap@gmail.comm>
2025-04-06 22:09:02 -0400

    changes to readme3231C

--- main.tf
+++ main.tf
@@ -24,6 +24,32 @@ provider "snowflake" {
   region      = var.SNOWFLAKE_REGION
 }
 
+variable "SNOWFLAKE_ACCOUNT" {
+  type        = string
+  description = "The Snowflake account identifier."
+}
+
+variable "SNOWFLAKE_USER" {
+  type        = string
+  description = "The Snowflake username."
+}
+
+variable "SNOWFLAKE_ROLE" {
+  type        = string
+  description = "The default Snowflake role."
+}
+
+variable "SNOWFLAKE_PRIVATE_KEY" {
+  type        = string
+  sensitive   = true
+  description = "The private key for Snowflake authentication."
+}
+
+variable "SNOWFLAKE_REGION" {
+  type        = string
+  description = "The Snowflake region (e.g., AWS_US_EAST_2)."
+}
+
 resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
