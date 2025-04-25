9bee2f468f44aeda4f6eb11b4c589c76a49c9e9e
pmalloja <mallojalap@gmail.comm>
2025-04-25 12:21:03 -0400

    added user5

--- main.tf
+++ main.tf
@@ -37,6 +37,8 @@ module "SALES_XSMALL_WH" {
   warehouse_size = "XSMALL"
 }
 
+
+
 module "ALL_USERS" {
   source = "./users"
   user_map = {
@@ -44,8 +46,8 @@ module "ALL_USERS" {
     TEST_USER2 = { "name" = "TEST_USER2", "first_name" = "test2", "last_name" = "user2", "email" = "user2@snowflake.example", "default_warehouse" = "COMPUTE_WH", "default_role" = "PUBLIC" }
     TEST_USER3 = { "name" = "TEST_USER3", "first_name" = "test3", "last_name" = "user3", "email" = "user3@snowflake.example" }
     TEST_USER4 = { "name" = "TEST_USER4", "first_name" = "test4", "last_name" = "user4", "email" = "user4@snowflake.example" }
+    TEST_USER5 = { "name" = "TEST_USER5", "first_name" = "test5", "last_name" = "user5", "email" = "user4@snowflake.example" }
 
   }
 }
-
 
